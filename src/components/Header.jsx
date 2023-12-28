1f92420220c13e5e4a5e3f5283e0e24718f6b917
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2023-12-27 20:59:28 -0800

    navbar responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 function Header() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+    
+  }
   return (
     <header>
       <div className="logo">
         <img src={require('../assets/images/logo.png')} alt="logoImage" />
       </div>
-      <div className="links">
+      <div className="links" ref={navRef} >
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#class">Our Classes</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="#pricing">Pricing</a></li>
-          <li><a href="#schedule">Schedule</a></li>
-          <li><a href="#gallery">Gallery</a></li>
-          <li><a href="#contact">Contacts</a></li>
+          <li onClick={showNavbar}><a href="#home">Home</a></li>
+          <li onClick={showNavbar}><a href="#about">About Us</a></li>
+          <li onClick={showNavbar}><a href="#class">Our Classes</a></li>
+          <li onClick={showNavbar}><a href="#team">Team</a></li>
+          <li onClick={showNavbar}><a href="#pricing">Pricing</a></li>
+          <li onClick={showNavbar}><a href="#schedule">Schedule</a></li>
+          <li onClick={showNavbar}><a href="#gallery">Gallery</a></li>
+          <li onClick={showNavbar}><a href="#contact">Contacts</a></li>
+          <i class="fa-solid fa-xmark" onClick={showNavbar}></i>
         </ul>
       </div>
-      <div className="hamburger">
-        <i class="fa-solid fa-bars"></i>
-      </div>
+      <i class="fa-solid fa-bars" onClick={showNavbar}></i>
     </header>
   )
 }
